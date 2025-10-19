0960f6e53df766392538c5b1308f87e4400fcc0e
jeff <sddeh5981@gmsil.com>
2025-10-19 11:57:58 +0300

    w

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (isLoggedIn) navigate("/"); // go home
+    else navigate("/login"); // send to login
+  }, [isLoggedIn]);
 
   return (
     <div className="app">
